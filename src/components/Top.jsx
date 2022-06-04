15cd88b63847fe33ea06addbe2004083f61e312c
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-06-04 10:12:08 +0300

    Complete!

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -7,31 +7,79 @@ import Logo from "../images/logo.svg";
 import Arrow from "../images/icon-arrow.svg";
 import Left from "../images/icon-angle-left.svg";
 import Right from "../images/icon-angle-right.svg";
+import Hamburger from "../images/icon-hamburger.svg";
+import Close from "../images/icon-close.svg";
 
 const Top = () => {
-  const images = [image1, image2, image3];
-  const [imageIndex, setImageIndex] = useState(0);
-  const [backgroundImage, setBackgroundImage] = useState();
+  const data = [
+    {
+      heading: "Discover innovative ways to decorate",
+      text: "We provide unmatched quality, comfort, and style for property owners across the country  Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+      image: image1,
+    },
+    {
+      heading: "We are available all across the globe",
+      text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+      image: image2,
+    },
+    {
+      heading: "Manufactured with the best materials",
+      text: " Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of     experience in this industry, we understand what customers want for their home and office.",
+      image: image3,
+    },
+  ];
+  const [dataIndex, setDataIndex] = useState(0);
+  const [shownData, setShownData] = useState(data[0]);
 
   const handleChangeImage = (direction) => {
-    if (direction === "left" && imageIndex > 0) {
-      setImageIndex((prevIndex) => prevIndex - 1);
+    if (direction === "left" && dataIndex > 0) {
+      setDataIndex((prevIndex) => prevIndex - 1);
     }
-    if (direction === "right" && imageIndex < images.length - 1) {
-      setImageIndex((prevIndex) => prevIndex + 1);
+    if (direction === "right" && dataIndex < data.length - 1) {
+      setDataIndex((prevIndex) => prevIndex + 1);
     }
   };
 
   useEffect(() => {
-    setBackgroundImage(images[imageIndex]);
-  }, [[], imageIndex]);
+    setShownData(data[dataIndex]);
+  }, [dataIndex]);
+
+  const [smallScreen, setSmallScreen] = useState(false);
 
   return (
     <div className="top-section flex">
       <div className="top-section-left">
-        <img src={backgroundImage} alt="Room" className="main-image" />
+        {smallScreen === true ? (
+          <div className="small-screen">
+            <div className="small-screen-menu flex">
+              <img
+                src={Close}
+                alt="Close"
+                onClick={() => {
+                  setSmallScreen((prevState) => !prevState);
+                }}
+              />
+              <ul className="small-screen-nav-links flex">
+                <li>home</li>
+                <li>shop</li>
+                <li>about</li>
+                <li>contact</li>
+              </ul>
+            </div>
+            <div className="smallScreen-overlay" />
+          </div>
+        ) : null}
+        <img src={shownData.image} alt="Room" className="main-image" />
         <nav className="flex">
-          <img src={Logo} alt="Logo" />
+          <img
+            src={Hamburger}
+            alt=""
+            className="hamburger"
+            onClick={() => {
+              setSmallScreen((prevState) => !prevState);
+            }}
+          />
+          <img src={Logo} alt="Logo" className="logo" />
           <ul className="nav-links flex">
             <li>home</li>
             <li>shop</li>
@@ -41,14 +89,8 @@ const Top = () => {
         </nav>
       </div>
       <div className="top-section-right">
-        <h2>Discover innovative ways to decorate</h2>
-        <p>
-          We provide unmatched quality, comfort, and style for property owners
-          across the country. Our experts combine form and function in bringing
-          your vision to life. Create a room in your own style with our
-          collection and make your property a reflection of you and what you
-          love.
-        </p>
+        <h2>{shownData.heading}</h2>
+        <p>{shownData.text}</p>
         <div className="flex">
           <p className="shop-now">SHOP NOW</p>
           <img src={Arrow} alt="Arrow" />
